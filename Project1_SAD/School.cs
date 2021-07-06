66544cda61fdda55c75259cb77b24e7d7a87c4c4
abdxax <abdxax>
2021-07-06 18:41:32 +0300

    project 1

---
+++ Project1_SAD/School.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project1_SAD
+{
+    class School
+    {
+        public int ID { get; set; }
+        public string _class { get; set; }
+        public string section { get; set; }
+
+        public School(int id,string cla,string section)
+        {
+            this.ID = id;
+            this._class = cla;
+            this.section = section;
+        }
+
+    }
+}
